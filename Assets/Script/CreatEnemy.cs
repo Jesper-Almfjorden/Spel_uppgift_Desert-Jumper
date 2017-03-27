0383ff7986a793c07fd2e7842ab613d6045120f5
Jesper Almfjorden <almfjorden16@gmail.com>
2017-03-27 16:23:44 +0200

    1 lesson left

---
+++ Assets/Script/CreatEnemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatEnemy : MonoBehaviour
+{
+    public GameObject enemy;
+    public Vector2 position;
+    public float timer;
+    private float startTimer;
+    // Use this for initialization
+    void Start()
+    {
+        startTimer = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 0)
+        {
+            position.x = Random.Range(10f, -10f);
+            Instantiate(enemy, position, Quaternion.identity);
+            timer = startTimer;
+
+        }
+
+        timer -= Time.deltaTime;
+    }
+}
---
+++ Assets/Script/Player_Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float jumpPower = 10f;
+    public bool grounded = false;
+
+    void Update()
+    {
+        if (!grounded && GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            grounded = true;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && grounded == true)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower);
+            grounded = false;
+        }
+        //Next two if statements are for moving left and right
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(new Vector2(1, 0) * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(new Vector2(-1, 0) * moveSpeed * Time.deltaTime);
+    }
+
+}
